a66cf53beb5bfd7b89c73827ef16c57a352ed0c0
TheKeons <katie.emblow@gmail.com>
2022-05-23 18:24:50 +0200

    Fixed static method and pep8

--- lesning_oop.py
+++ lesning_oop.py
@@ -8,10 +8,13 @@ os.chdir(sys.path[0])
 
 # Gets norges-befolkning.csv and puts the information into a 2D array named "data"
 with open('norges-befolkning.csv', 'r') as f:
-    norway = np.genfromtxt(f, delimiter=';', dtype=int, skip_header=1, usecols=[0, 1], unpack=True)   # returns a 2d array
+    norway = np.genfromtxt(f, delimiter=';', dtype=int, skip_header=1, usecols=[0, 1], unpack=True)   
+    # returns a 2d array
     
 with open('sveriges-befolkning.csv', 'r') as f:
-    sweden = np.genfromtxt(f, delimiter=';', dtype=int, skip_header=1, usecols=[0, 1], unpack=True)   # returns a 2d array
+    sweden = np.genfromtxt(f, delimiter=';', dtype=int, skip_header=1, usecols=[0, 1], unpack=True)   
+    # returns a 2d array
+
 
 class Placeholder:
     def __init__(self, file, file2):
@@ -32,30 +35,27 @@ class Placeholder:
         self.average_growth_rate_15 = (self.y[-1] - self.y[len(self.y) - 15]) / (self.x[-1] - self.x[len(self.y) - 15])
         self.average_growth_rate_5 = (self.y[-1] - self.y[len(self.y) - 5]) / (self.x[-1] - self.x[len(self.y) - 5])
 
-        self.average_growth_rate_30_2 = (self.k[-1] - self.k[len(self.k) - 30]) / (self.t[-1] - self.t[len(self.k) - 30])
-        self.average_growth_rate_15_2 = (self.k[-1] - self.k[len(self.k) - 15]) / (self.t[-1] - self.t[len(self.k) - 15])
+        self.average_growth_rate_30_2 = (self.k[-1] - self.k[len(self.k) - 30]) / \
+                                        (self.t[-1] - self.t[len(self.k) - 30])
+        self.average_growth_rate_15_2 = (self.k[-1] - self.k[len(self.k) - 15]) / \
+                                        (self.t[-1] - self.t[len(self.k) - 15])
         self.average_growth_rate_5_2 = (self.k[-1] - self.k[len(self.k) - 5]) / (self.t[-1] - self.t[len(self.k) - 5])
         
         # Gets list of y-values to draw prediction lines
         self.x_prediction = range(2022, 2072)
-        self.pred_30 = [self.point_slope_form(self.average_growth_rate_30, self.x1, self.y1)(i) for i in self.x_prediction]
-        self.pred_15 = [self.point_slope_form(self.average_growth_rate_15, self.x1, self.y1)(i) for i in self.x_prediction]
-        self.pred_5 = [self.point_slope_form(self.average_growth_rate_5, self.x1, self.y1)(i) for i in self.x_prediction]
+        self.pred_30 = [point_slope_form(self.average_growth_rate_30, self.x1, self.y1)(i) for i in self.x_prediction]
+        self.pred_15 = [point_slope_form(self.average_growth_rate_15, self.x1, self.y1)(i) for i in self.x_prediction]
+        self.pred_5 = [point_slope_form(self.average_growth_rate_5, self.x1, self.y1)(i) for i in self.x_prediction]
 
-        self.pred_30_2 = [self.point_slope_form(self.average_growth_rate_30_2, self.t1, self.k1)(i) for i in self.x_prediction]
-        self.pred_15_2 = [self.point_slope_form(self.average_growth_rate_15_2, self.t1, self.k1)(i) for i in self.x_prediction]
-        self.pred_5_2 = [self.point_slope_form(self.average_growth_rate_5_2, self.t1, self.k1)(i) for i in self.x_prediction]
-
-    # Returns the derivative for all values of x in a list
-    def der(self, x, y):
-        return [(y[i+1] - y[i]) / (x[i+1] - x[i]) for i in range(len(y) - 1)]
-
-    # Returns a linear function using the point slope formula
-    def point_slope_form(self, growth_rate, x1, y1):
-        return lambda x: growth_rate * (x - x1) + y1
+        self.pred_30_2 = [point_slope_form(self.average_growth_rate_30_2,
+                                           self.t1, self.k1)(i) for i in self.x_prediction]
+        self.pred_15_2 = [point_slope_form(self.average_growth_rate_15_2,
+                                           self.t1, self.k1)(i) for i in self.x_prediction]
+        self.pred_5_2 = [point_slope_form(self.average_growth_rate_5_2, self.t1, 
+                                          self.k1)(i) for i in self.x_prediction]
 
     # Chooses which type of graph to make
-    def plot(self, graph_type, goal, populations, names):  
+    def plot(self, graph_type, goal, populations, names: tuple):
         match graph_type:
             case 1:
                 self.plot_prediction(goal, populations, names)
@@ -102,14 +102,14 @@ class Placeholder:
     def plot_derivative(self, goal, populations, names):
         match populations:
             case 'uno':
-                plt.plot(self.x[1:], [i * 1000000 for i in self.der(self.x, self.y)])
+                plt.plot(self.x[1:], [i * 1000000 for i in der(self.x, self.y)])
             
             case 'dos':
-                plt.plot(self.t[1:], [i * 1000000 for i in self.der(self.t, self.k)])
+                plt.plot(self.t[1:], [i * 1000000 for i in der(self.t, self.k)])
             
             case 'both':
-                plt.plot(self.x[1:], [i * 1000000 for i in self.der(self.x, self.y)], label=names[0])
-                plt.plot(self.t[1:], [i * 1000000 for i in self.der(self.t, self.k)], label=names[1])
+                plt.plot(self.x[1:], [i * 1000000 for i in der(self.x, self.y)], label=names[0])
+                plt.plot(self.t[1:], [i * 1000000 for i in der(self.t, self.k)], label=names[1])
         # times with a million so that the growth rate is accurate to the actual population
         plt.title('Population growth rate')
         plt.xlabel('Year')
@@ -143,6 +143,16 @@ class Placeholder:
             plt.savefig('population.png')
         else:
             plt.show()
+
+
+# Returns the derivative for all values of x in a list
+def der(x, y):
+    return [(y[i+1] - y[i]) / (x[i+1] - x[i]) for i in range(len(y) - 1)]
+
+
+# Returns a linear function using the point slope formula
+def point_slope_form(growth_rate, x1, y1):
+    return lambda x: growth_rate * (x - x1) + y1
 
 
 # Instantiates the class and picks a setting
